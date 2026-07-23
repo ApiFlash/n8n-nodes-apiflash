7825794a99eccc1743988c7f7e39a2884392ef5f
Timothée Jeannin <tjeannin@netcube.fr>
2026-07-23 15:29:58 +0200

    Use ApiFlash blue logo for node icon

--- credentials/ApiflashApi.credentials.ts
+++ credentials/ApiflashApi.credentials.ts
@@ -1,49 +1,46 @@
 import type {
-	IAuthenticateGeneric,
-	ICredentialTestRequest,
-	ICredentialType,
-	Icon,
-	INodeProperties,
+    IAuthenticateGeneric,
+    ICredentialTestRequest,
+    ICredentialType,
+    Icon,
+    INodeProperties,
 } from 'n8n-workflow';
 
 export class ApiflashApi implements ICredentialType {
-	name = 'apiflashApi';
+    name = 'apiflashApi';
 
-	displayName = 'ApiFlash API';
+    displayName = 'ApiFlash API';
 
-	icon: Icon = {
-		light: 'file:../nodes/Apiflash/apiflash.svg',
-		dark: 'file:../nodes/Apiflash/apiflash.dark.svg',
-	};
+    icon: Icon = 'file:../nodes/Apiflash/apiflash.svg';
 
-	documentationUrl = 'https://github.com/ApiFlash/n8n-nodes-apiflash?tab=readme-ov-file#credentials';
+    documentationUrl = 'https://github.com/ApiFlash/n8n-nodes-apiflash?tab=readme-ov-file#credentials';
 
-	properties: INodeProperties[] = [
-		{
-			displayName: 'Access Key',
-			name: 'accessKey',
-			type: 'string',
-			typeOptions: { password: true },
-			required: true,
-			default: '',
-			description:
-				'Your ApiFlash access key. You can find it in your <a href="https://apiflash.com/dashboard/access_keys">ApiFlash dashboard</a>.',
-		},
-	];
+    properties: INodeProperties[] = [
+        {
+            displayName: 'Access Key',
+            name: 'accessKey',
+            type: 'string',
+            typeOptions: {password: true},
+            required: true,
+            default: '',
+            description:
+                'Your ApiFlash access key. You can find it in your <a href="https://apiflash.com/dashboard/access_keys">ApiFlash dashboard</a>.',
+        },
+    ];
 
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			qs: {
-				access_key: '={{$credentials.accessKey}}',
-			},
-		},
-	};
+    authenticate: IAuthenticateGeneric = {
+        type: 'generic',
+        properties: {
+            qs: {
+                access_key: '={{$credentials.accessKey}}',
+            },
+        },
+    };
 
-	test: ICredentialTestRequest = {
-		request: {
-			baseURL: 'https://api.apiflash.com/v1',
-			url: '/urltoimage/quota',
-		},
-	};
+    test: ICredentialTestRequest = {
+        request: {
+            baseURL: 'https://api.apiflash.com/v1',
+            url: '/urltoimage/quota',
+        },
+    };
 }
--- nodes/Apiflash/resources/screenshot/index.ts
+++ nodes/Apiflash/resources/screenshot/index.ts
@@ -1,37 +1,37 @@
-import type { INodeProperties } from 'n8n-workflow';
-import { screenshotCaptureDescription } from './capture';
+import type {INodeProperties} from 'n8n-workflow';
+import {screenshotCaptureDescription} from './capture';
 
 const showOnlyForScreenshots = {
-	resource: ['screenshot'],
+    resource: ['screenshot'],
 };
 
 export const screenshotDescription: INodeProperties[] = [
-	{
-		displayName: 'Operation',
-		name: 'operation',
-		type: 'options',
-		noDataExpression: true,
-		displayOptions: {
-			show: showOnlyForScreenshots,
-		},
-		options: [
-			{
-				name: 'Capture',
-				value: 'capture',
-				action: 'Capture a screenshot',
-				description: 'Capture a screenshot of a website',
-				routing: {
-					request: {
-						method: 'GET',
-						url: '/urltoimage',
-						qs: {
-							response_type: 'json',
-						},
-					},
-				},
-			},
-		],
-		default: 'capture',
-	},
-	...screenshotCaptureDescription,
+    {
+        displayName: 'Operation',
+        name: 'operation',
+        type: 'options',
+        noDataExpression: true,
+        displayOptions: {
+            show: showOnlyForScreenshots,
+        },
+        options: [
+            {
+                name: 'Capture',
+                value: 'capture',
+                action: 'Capture a screenshot',
+                description: 'Capture a screenshot of a website',
+                routing: {
+                    request: {
+                        method: 'GET',
+                        url: '/urltoimage',
+                        qs: {
+                            response_type: 'json',
+                        },
+                    },
+                },
+            },
+        ],
+        default: 'capture',
+    },
+    ...screenshotCaptureDescription,
 ];
